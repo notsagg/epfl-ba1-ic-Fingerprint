e8725b9a7657fbf24bf3a1ef4d51c34bad200d0c
Gaston Pelletier <gaston@pelletier.se>
2021-11-13 18:39:34 +0100

    chore(build): update build scripts

--- src/cs107/build/getJunit.sh
+++ src/cs107/build/getJunit.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # 0. ensure that the script is being run from the build directory
-if [ $(dirname "$0") != "./build/deps/contrib"  ]; then
+if [ $(dirname "$0") != "."  ]; then
     echo "error: script is not being run from the build directory"
     exit 1
 fi
 
 # 1. download the archive file
 echo "1/2 - downloading jUnit archive file"
-wget https://github.com/downloads/junit-team/junit/junit-4.10.jar -P deps/junit
+wget https://repo1.maven.org/maven2/junit/junit/4.13.2/junit-4.13.2.jar -P deps/junit
 
-# 2. set junit .jar path
+# 2. set the junit .jar path
 echo "2/2 - setting environment variables"
-export JUNIT_HOME=$(pwd)/deps/junit
-export CLASSPATH=$CLASSPATH:$JUNIT_HOME/junit-4.10.jar:.
+echo "export JUNIT_HOME=$(pwd)/deps/junit" >> ~/.bashrc
+echo "export CLASSPATH=$CLASSPATH:$JUNIT_HOME/junit-4.13.2.jar:." >> ~/.bashrc
+
+# 3. reload the .bashrc configuration file
+source ~/.bashrc
