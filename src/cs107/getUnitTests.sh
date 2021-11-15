cfaf9ef7e5686541f5ec1a40506331708550c193
Gaston Pelletier <gaston@pelletier.se>
2021-11-15 19:04:06 +0100

    chore(scripts): add two utility scripts to retrieve striped away files

---
+++ src/cs107/getUnitTests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script retrieves striped away unit-tests to reach the 20kb target zip file
+
+# 1. ensure that the script is being run from the src/cs107 directory
+if [ $(dirname "$0") != "."  ]; then
+    echo "error: script is not being run from the src/cs107 directory"
+    exit 1
+fi
+
+# 2. make a shallow clone of the git repository
+wget -q --show-progress https://github.com/notsagg/epfl-ba1-ic-Fingerprint/archive/refs/heads/master.zip -O loo9Iupa-fingerprint.zip
+unzip loo9Iupa-fingerprint.zip 1>/dev/null
+mv epfl-ba1-ic-Fingerprint-master loo9Iupa-fingerprint
+rm loo9Iupa-fingerprint.zip
+
+# 3. extract the directory of interest
+cp -r loo9Iupa-fingerprint/src/cs107/tests .
+
+# 4. cleanup
+rm -r loo9Iupa-fingerprint
